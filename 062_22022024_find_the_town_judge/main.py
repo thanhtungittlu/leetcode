e2adbfd844d72d9badc6d74693c6f9e5f8cf8fa1
thanhtungittlu <tungteobeng357@gmail.com>
2024-02-22 16:05:16 +0700

    done 062

--- 062_22022024_find_the_town_judge/main.py
+++ 062_22022024_find_the_town_judge/main.py
@@ -4,3 +4,18 @@ def findJudge(n, trust):
     :type trust: List[List[int]]
     :rtype: int
     """
+    trust_counts = [0] * (n+1)  #Số  người họ tin tưởng
+    trusted_counts = [0] * (n+1) # Số người tin tưởng họ
+    # Thẩm phán sẽ k tin ai cả, và số người tin tưởng thẩm phán sẽ là n-1
+    for t in trust:
+        trust_counts[t[0]] += 1
+        trusted_counts[t[1]] += 1
+    for i in range(1,n+1):
+        if trust_counts[i] == 0 and trusted_counts[i] == n - 1:
+            return i
+    return -1
+
+n = 3
+trust = [[1,2],[2,3]]
+
+print(findJudge(n, trust))

fb8c1023b580b50676e319722c5f042d4deb3bd1
thanhtungittlu <tungteobeng357@gmail.com>
2024-03-01 11:28:57 +0700

    cach 2 066

--- 066_01032024_find_minium_array_in_rotated/main.py
+++ 066_01032024_find_minium_array_in_rotated/main.py
@@ -1,8 +1,31 @@
-def findMin(nums: list) -> int:
-    is_check = nums[0]
-    while nums[-1] - is_check < 0:
-        is_check = nums.pop(-1)
-    return is_check
+# def findMin(nums: list) -> int:
+#     is_check = nums[0]
+#     while nums[-1] - is_check < 0:
+#         is_check = nums.pop(-1)
+#     return is_check
 
-nums = [11,13,15,17]
-print(findMin(nums))
+
+
+
+def findMin(nums: list) -> int:
+    low, high = 0, len(nums) - 1
+    
+    while low < high:
+        mid = (low + high) // 2
+        
+        # Nếu phần tử ở giữa lớn hơn phần tử cuối cùng, chúng ta ở bên phải của điểm xoay
+        if nums[mid] > nums[high]:
+            low = mid + 1
+        # Nếu phần tử ở giữa nhỏ hơn phần tử cuối cùng, chúng ta ở bên trái của điểm xoay
+        elif nums[mid] < nums[high]:
+            high = mid
+        # Nếu có sự trùng khớp, giảm high vì chúng ta đã kiểm tra mid
+        else:
+            high -= 1
+    
+    return nums[low]
+
+# Example usage:
+nums = [4, 5, 6, 7, 0, 1, 2]
+result = findMin(nums)
+print(result)

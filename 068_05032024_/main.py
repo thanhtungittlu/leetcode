704a36de948a9443e12e176728e674fc46685e40
thanhtungittlu <tungteobeng357@gmail.com>
2024-03-05 10:03:39 +0700

    done 068

---
+++ 068_05032024_/main.py
@@ -0,0 +1,31 @@
+def search(nums: list, target: int) -> int:
+    left, right = 0, len(nums) - 1
+    
+    while left <= right:
+        mid = (left + right) // 2
+        
+        if nums[mid] == target:
+            return mid
+        
+        # Check if the left half is sorted
+        if nums[left] <= nums[mid]:
+            if nums[left] <= target <= nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        # If the left half is not sorted, then the right half must be sorted
+        else:
+            if nums[mid] <= target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+    
+    return -1
+
+nums = [4,5,6,7,0,1,2]
+target = 0
+
+
+print(search(nums, target))
+
+
